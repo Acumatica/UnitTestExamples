0580e50cb482e2abf4e8b4e64bdb2e876828a7a2
dnaumov <dnaumov@acumatica.com>
2020-07-22 15:34:34 -0400

    Update with Example from Virtual Dev Conference 2020

---
+++ UnitTestsDemo/APBillsTest.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Autofac;
+using System.Collections;
+using System.Linq;
+
+using Xunit;
+
+using PX.Data;
+using PX.Data.Unit;
+
+using PX.Objects.GL;
+using PX.Objects.AP;
+using PX.Objects.GL.FinPeriods;
+using PX.Objects.CM.Extensions;
+
+namespace VirtualDevConfDemo
+{
+    public class APBillsTest : TestBase
+    {       
+        protected override void RegisterServices(ContainerBuilder builder)
+        {
+            base.RegisterServices(builder);
+            builder.RegisterType<PX.Objects.Unit.FinPeriodServiceMock>().As<IFinPeriodRepository>();
+            builder.RegisterType<PX.Objects.Unit.CurrencyServiceMock>().As<IPXCurrencyService>();
+        }
+
+        private APInvoiceEntry PrepareGraph()
+        {
+            Setup<APInvoiceEntry>(
+                new GLSetup
+                {
+                    RequireControlTotal = false
+                });
+            Setup<APInvoiceEntry>(
+                new APSetup
+                {
+                    RequireControlTotal = false
+                });
+            var graph = PXGraph.CreateInstance<APInvoiceEntry>();
+            return graph;
+        }
+
+        [Fact]
+        public void Test_Defaulting_CuryIDFromVendor()
+        {
+            // Create and set up object you are going to test
+            APInvoiceEntry graph = PrepareGraph();
+
+            graph.Caches[typeof(PX.Objects.CM.CurrencyList)].Insert(
+             new PX.Objects.CM.CurrencyList
+             {
+                 CuryID = "SGD",
+                 IsFinancial = true
+             });
+
+            Vendor vendor =
+             (Vendor)graph.Caches[typeof(Vendor)].Insert(
+                 new Vendor { 
+                     AcctCD= "TESTVENDOR",
+                     AcctName = "Test Vendor",
+                     CuryID = "SGD" 
+                 });
+
+
+            // Execute the action that you will test
+            var bill = graph.Document.Insert();
+            bill.VendorID = vendor.BAccountID;
+            bill = graph.Document.Update(bill);
+
+
+            // Check the result
+            Assert.Equal("SGD", bill.CuryID);
+        }
+    }
+}
---
+++ UnitTestsDemo/CATransferTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Autofac;
+using Xunit;
+using PX.Data;
+using PX.Data.Unit;
+using PX.Objects.GL;
+using PX.Objects.CA;
+using PX.Objects.GL.FinPeriods;
+using PX.Objects.CM.Extensions;
+
+namespace SummitDemo
+{
+    public class CATransferTest : TestBase
+    {       
+        protected override void RegisterServices(ContainerBuilder builder)
+        {
+            base.RegisterServices(builder);
+            builder.RegisterType<PX.Objects.Unit.FinPeriodServiceMock>().As<IFinPeriodRepository>();
+            builder.RegisterType<PX.Objects.Unit.CurrencyServiceMock>().As<IPXCurrencyService>();
+        }
+
+        private CashTransferEntry PrepareGraph()
+        {
+            Setup<CashTransferEntry>(
+                new GLSetup
+                {
+                    YtdNetIncAccountID = int.MaxValue - 2,
+                    RetEarnAccountID = int.MaxValue - 1,
+                    RequireControlTotal = false
+                });
+            Setup<CashTransferEntry>(
+                new CASetup
+                {
+                    RequireControlTotal = false
+                });
+            var graph = PXGraph.CreateInstance<CashTransferEntry>();
+            return graph;
+        }
+
+        [Fact]
+        public void Test_Defaulting_InCuryID()
+        {
+            CashTransferEntry graph = PrepareGraph();
+
+            CashAccount cashaccount =
+             (CashAccount)graph.Caches[typeof(CashAccount)].Insert(
+                 new CashAccount { 
+                     CashAccountCD = "102000", 
+                     CuryID = "USD" 
+                 });
+
+            var transfer = graph.Transfer.Insert();
+            transfer.InAccountID = cashaccount.CashAccountID;
+            transfer = graph.Transfer.Update(transfer);
+
+            Assert.Equal("USD", transfer.InCuryID);
+        }
+    }
+}
